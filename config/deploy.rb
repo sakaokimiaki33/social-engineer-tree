18a86d3f260f245621736da460bb1ecb64f2a9c1
sakaokimiaki <sakaokimiaki33@yahoo.co.jp>
2020-06-07 20:34:52 +0900

    cont

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,7 @@ lock '3.14.0'
 set :application, 'social-engineer-tree'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sakaokimiaki33/social-engineer-tree
-.git'
+set :repo_url,  'git@github.com:sakaokimiaki33/social-engineer-tree.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
